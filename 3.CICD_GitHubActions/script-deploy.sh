d6e1cdde73604f849074d67e887302964174bba9
moronmon <59859392+moronmon@users.noreply.github.com>
2025-03-01 16:50:38 +0700

    Create script-deploy.sh

---
+++ 3.CICD_GitHubActions/script-deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose -f /home/userdeploy/2.Container-Docker/docker-compose.yaml pull
+docker-compose -f /home/userdeploy/2.Container-Docker/docker-compose.yaml up -d
