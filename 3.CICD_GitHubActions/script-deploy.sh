538fd5987b79e66532918965f79ce3485c878641
moronmon <59859392+moronmon@users.noreply.github.com>
2025-03-01 17:06:58 +0700

    Update script-deploy.sh

--- 3.CICD_GitHubActions/script-deploy.sh
+++ 3.CICD_GitHubActions/script-deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-# docker-compose -f /home/userdeploy/2.Container-Docker/docker-compose.yaml pull
-# docker-compose -f /home/userdeploy/2.Container-Docker/docker-compose.yaml up -d
-docker-compose --version
+docker-compose -f /home/userdeploy/apps/docker-compose.yaml pull
+docker-compose -f /home/userdeploy/apps/docker-compose.yaml up -d
